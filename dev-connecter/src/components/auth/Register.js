600b6baabf16574c700ab7ebceb3e718193b709e
Nadish Maredia <Nadish Maredia@NADISH>
2021-01-29 19:40:23 +0500

    Alert Actions

--- dev-connecter/src/components/auth/Register.js
+++ dev-connecter/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-function Register() {
+function Register({ setAlert }) {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -19,7 +22,7 @@ function Register() {
     const onSubmit = async (e) => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Password donot match');
+            setAlert('Password donot match', 'danger');
         } else {
             console.log('success');
             // const newUser = {
@@ -100,6 +103,10 @@ function Register() {
             </p>
         </div>
     )
-}
+};
 
-export default Register;
+Register.propTypes = {
+    setAlert:PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
